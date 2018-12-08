ac7bf11ab8e4409859e7764e6e01573464c8b311
montanel <luca.montanelli@bluewin.ch>
2018-12-08 14:20:04 +0100

    started hyperparameters opt and tweaked 0_clenaing.py

--- scripts/0_cleaning.py
+++ scripts/0_cleaning.py
@@ -16,6 +16,7 @@ sc = spark.sparkContext
 WLP_FILE = '/datasets/now_corpus/corpus/wlp/*-*-*.txt'
 #WLP_FILE = '../*-*-*.txt'
 NPARTITION = 40
+BOTTOM_PERCENT = 0.8
 TOP_PERCENT = 0.95
 
 
@@ -29,13 +30,13 @@ lines = wlp_rdd.map(lambda r: r.split('\t'))
 
 #identify the columns
 wlp_schema = lines.map(lambda r: Row(textID=int(r[0]),idseq=int(r[1]),word=r[2],lemma=r[3],pos=r[4]))
-wlp = spark.createDataFrame(wlp_schema) #.repartition(NPARTITION,'lemma').persist() #this partition propagates and so does not need to be repeated
+wlp = spark.createDataFrame(wlp_schema).repartition(NPARTITION,'lemma').persist() #this partition propagates and so does not need to be repeated
 
 
 ############## Cleaning stage ##############
 #pos
 pos_remove = ['.',',',"\'",'\"','null']
-wlp_nopos = wlp.filter(~wlp['pos'].isin(pos_remove)).filter(~wlp['pos'].startswith('m')).filter(~wlp['pos'].startswith('f')).drop('idseq','pos','word')
+wlp_nopos = wlp.filter(~wlp['pos'].isin(pos_remove)).filter(~wlp['pos'].startswith('m')).filter(~wlp['pos'].startswith('f')).filter(~wlp['pos'].startswith('np')).drop('idseq','pos','word')
 
 #stopwords
 stopwords = sc.textFile('our_stopwords.txt').collect()
@@ -43,8 +44,8 @@ wlp_nostop = wlp_nopos.filter(~wlp['lemma'].isin(stopwords))
 lemma_freq = wlp_nostop.groupBy('lemma').count()
 
 #removal
-[top] = lemma_freq.approxQuantile('count', [TOP_PERCENT], 0.01)
-lemma_tokeep = lemma_freq.filter(lemma_freq['count']<top)
+[bottom, top] = lemma_freq.approxQuantile('count', [BOTTOM_PERCENT, TOP_PERCENT], 0.01)
+lemma_tokeep = lemma_freq.filter(lemma_freq['count']<top).filter(lemma_freq['count']>bottom)
 
 wlp_nostop.registerTempTable('wlp_nostop')
 lemma_tokeep.registerTempTable('lemma_tokeep')
@@ -56,7 +57,7 @@ INNER JOIN lemma_tokeep ON wlp_nostop.lemma = lemma_tokeep.lemma
 """
 
 wlp_kept = spark.sql(query)
-wlp_bytext = wlp_kept.groupBy('textID').agg(collect_list('lemma')).sort('textID').withColumnRenamed('collect_list(lemma)','lemma_list')
+wlp_bytext = wlp_kept.groupBy('textID').agg(collect_list('lemma')).sort('textID').withColumnRenamed('collect_list(lemma)','lemma_list').repartition(NPARTITION,'lemma').persist()
 
 
 
